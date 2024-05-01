Refactor matrix normalization, add fixed-point conversion, and update convolution result printing in Calculator.py

CalculateProcess/Calculator.py
@@ -53,19 +53,45 @@ print("Result of the convolution:\n")
 np.set_printoptions(formatter={'float': '{: 0.10f}'.format}, suppress=True)
 print(str(result))
 
+#根據a.的floating-point運算內容，得出其fixed-point值的運算結果(8-bit  16-bit)，
+# 並計算其SQNR(由36個output值做平均後計算SQNR，SQNR值需 > 30 dB)。(10%)
+
+# Calculate the Signal-to-Quantization Noise Ratio (SQNR)
+def calculate_sqnr(matrix, result):
+    # Calculate the quantization noise
+    quantization_noise = ( np.sum(matrix)/len(matrix) - np.sum(result)/len(result) ) ** 2
+    
+    # Calculate the signal power
+    signal_power = np.sum(matrix**2)
+    
+    # Calculate the SQNR
+    sqnr = 10 * np.log10(signal_power / quantization_noise)
+    
+    return sqnr
+
+sqnr = calculate_sqnr(matrix, result)
+
+print("\nSQNR: " + str(sqnr) + " dB")
+
+
 print("\n---------------------------------------------------------------------------------------\n")
+
+matrix = matrix.tolist()
+
+
+print("Fixed-point 8-bit matrix:\n")
 
 for i in range(len(matrix)):
     for j in range(len(matrix[0])):
         matrix[i][j] = tf.float_to_fixed_point_8(matrix[i][j])
-
-print("Fixed-point 8-bit matrix:\n")
-print(matrix)
+    print(matrix[i])
 
 
+print("Fixed-point 16-bit result:\n")
+result = result.tolist()
 for i in range(len(result)):
     for j in range(len(result[0])):
         result[i][j] = tf.float_to_fixed_point_16(result[i][j])
+    print(result[i])
 
-print("Fixed-point 16-bit result:\n")
-print(result)
+

CalculateProcess/kernel.py
@@ -2,4 +2,5 @@ import numpy as np
 
 # Initialize a 3x3 convolutional kernel with values from a uniform distribution between -0.1 and 0.1
 kernel = np.random.uniform(-1, 1, (3, 3))
-print(kernel)
+kernel = kernel.tolist()
+print(kernel, sep = ',')
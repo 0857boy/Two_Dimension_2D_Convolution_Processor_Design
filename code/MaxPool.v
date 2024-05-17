b34155bf4ffe1d15d4e46bf01227e839ee73e912
Joe Liao <57323728+0857boy@users.noreply.github.com>
2024-05-17 10:52:58 +0800

    refactor: Update MaxPool module to handle additional input channels

--- code/MaxPool.v
+++ code/MaxPool.v
@@ -1,10 +1,12 @@
 module MaxPool(din1, din2, din3, din4, dout);
 
-input [15:0] din1, din2, din3, din4;
+input [15:0] din1, din2, din3, din4, din5, din6;
 output [15:0] dout;
 
 
-assign dout = (din1 > din2 && din1 > din3 && din1 > din4) ? din1 : 
-              (din2 > din3 && din2 > din4) ? din2 : 
-              (din3 > din4) ? din3 : din4;
+assign dout = (din1 > din2 && din1 > din3 && din1 > din4 && din1 > din5 && din1 > din6) ? din1 : 
+              (din2 > din3 && din2 > din4 && din2 > din5 && din2 > din6) ? din2 : 
+              (din3 > din4 && din3 > din5 && din3 > din6) ? din3 : 
+              (din4 > din5 && din4 > din6) ? din4 : 
+              (din5 > din6) ? din5 : din6;
 endmodule
